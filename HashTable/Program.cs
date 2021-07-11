795022d603b32f33ee2777b5806e1422a173094a
[Sona] <[sona16061999@gmail.com]>
2021-07-12 00:08:43 +0530

    [Sona] HashTable is Created

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -7,6 +7,15 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to HashTable Programs");
+
+            Hash<string, int> hash = new Hash<string, int>(6);
+            hash.AddNode("To", 1);
+            hash.AddNode("be", 1);
+            hash.AddNode("or", 1);
+            hash.AddNode("not", 1);
+            hash.AddNode("To", 1);
+            hash.AddNode("be", 1);
+            hash.Display();
         }
     }
 }
